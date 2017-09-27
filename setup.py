6bb893354af2104d0fbd0d91d408a9c1ab3da528
Samapriya Roy <samapriya.roy@gmail.com>
2017-09-27 07:19:11 -0400

    Updated to v0.1.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='pclip',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     url='https://github.com/samapriya/Clip-Ship-Planet-CLI',
     license='Apache 2.0',
