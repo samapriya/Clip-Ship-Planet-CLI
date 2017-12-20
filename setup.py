a920ddb6a4a51cc0ca8124bb053e0a1fb1fab743
Samapriya Roy <samapriya.roy@gmail.com>
2017-12-20 01:11:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='pclip',
-    version='0.2.0',
+    version='0.2.1',
     packages={'pclip': ['aoi.json']},
     package_data={'': ['aoi.json']},
     url='https://github.com/samapriya/Clip-Ship-Planet-CLI',
